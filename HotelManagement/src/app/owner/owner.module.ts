db2070640b72953d407134957c0ac5a68f3c055a
Tanuja <thetetanuja@gmail.com>
2023-08-15 19:45:59 +0530

    material installed

--- HotelManagement/src/app/owner/owner.module.ts
+++ HotelManagement/src/app/owner/owner.module.ts
@@ -3,11 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { OwnerRoutingModule } from './owner-routing.module';
 import { OwnerhomeComponent } from './ownerhome/ownerhome.component';
+import { OwnersignupComponent } from './ownersignup/ownersignup.component';
+import { OwnersuccessComponent } from './ownersuccess/ownersuccess.component';
+import { NewhotelregistrationComponent } from './newhotelregistration/newhotelregistration.component';
+ 
 
 
 @NgModule({
   declarations: [
-    OwnerhomeComponent
+    OwnerhomeComponent,
+    OwnersignupComponent,
+    OwnersuccessComponent,
+    NewhotelregistrationComponent,
+    
   ],
   imports: [
     CommonModule,
